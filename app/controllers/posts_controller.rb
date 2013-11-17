57888dd34bd125f9ec0545e6c4ee3fa3a1e0097c
Rick Cid <rick@hispand.com>
2013-11-17 01:14:50 -0700

    Added view templates and adjusted navigation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
   def index
     @post = Post.all
   end
@@ -12,13 +14,37 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(params[:post])
+    @post = Post.new(post_params)
+    @post.user = User.first
+
+    if @post.save
+      flash[:notice] = "Exito, has publicado!"
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @post.update(post_params)
+      flash[:notice] = "Exito, has actualizado!"
+      redirect_to posts_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :url, :description)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 end
