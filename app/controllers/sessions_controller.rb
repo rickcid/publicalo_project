38a05c9cb38f8bb29d5a1fb4654890cb3aecc852
Rick Cid <rick@hispand.com>
2013-11-23 23:30:17 -0700

    Made additions to user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    flash[:notice] = "Has cerrado tu sesion."
+    flash[:notice] = "Se ha cerrado tu sesion."
     redirect_to root_path
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def new
     @user = User.new
   end
@@ -15,17 +17,28 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+  end
+
   def edit
-    
   end
 
   def update
-    
+    if @user.update(user_params)
+      flash[:notice] = "Tu perfil fue actualizado."
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
   end
 
   private
 
   def user_params
     params.require(:user).permit(:username, :password)  
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
