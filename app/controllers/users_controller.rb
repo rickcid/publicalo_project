01adb11c25711316778e196f3a4117b64c11a85b
Rick Cid <rick@hispand.com>
2013-11-24 23:33:06 -0700

    Enhanced user profile view page with activity tabs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
 
   def new
     @user = User.new
@@ -40,5 +41,12 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:error] = "No es permitido realizar esta accion!"
+      redirect_to root_path
+    end
   end
 end
