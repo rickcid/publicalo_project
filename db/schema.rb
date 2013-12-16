86bd07847b2fe60128eb03f203877b44d4a71307
Rick Cid <rick@hispand.com>
2013-12-15 22:42:54 -0700

    Added slugging to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131215063413) do
+ActiveRecord::Schema.define(version: 20131216051552) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20131215063413) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "slug"
   end
 
   create_table "votes", force: true do |t|
