160024167687d25aaa14624c14e4a9192f87c77c
Rick Cid <rick@hispand.com>
2013-12-17 12:00:23 -0700

    Corrected category slug column reference

--- app/models/category.rb
+++ app/models/category.rb
@@ -7,7 +7,7 @@ class Category < ActiveRecord::Base
   before_save :generate_slug
 
   def generate_slug
-    self.slug = self.title.gsub(' ', '-').downcase
+    self.slug = self.name.gsub(' ', '-').downcase
   end 
 
   def to_param
