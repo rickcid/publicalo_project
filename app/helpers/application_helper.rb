75a51ebe81c9c2f8336a7a05c60396ff7b4e34e8
Rick Cid <rick@hispand.com>
2013-12-18 23:51:28 -0700

    Made a correction to time zone code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
 
   def display_datetime(dt)
     if logged_in? && !current_user.time_zone.blank?
-      dt = dt.in_time_zone(current.user.time_zone)
+      dt = dt.in_time_zone(current_user.time_zone)
     end
     dt.strftime("%d/%m/%Y %l:%M%P %Z")
   end
